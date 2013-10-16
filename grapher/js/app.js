ecb2fdd5d0b158329a21b313fbffa3154c10f869
Jonathan Goldsmith <jhghank@gmail.com>
2013-10-15 23:03:50 -0700

    add underscore to the global context

--- grapher/js/app.js
+++ grapher/js/app.js
@@ -1,11 +1,17 @@
-'use strict';
-
 var app = angular.module('grapherApp', ['grapherApp.services', 'grapherApp.directives']);
 
 app.controller("GrapherCtrl", function($scope, diffEq) {
   $scope.y_0 = 0;
 
+  $scope.slope_func = function(x, y) {
+    return x*Math.cos(y);
+  };
+
   $scope.$watch("y_0", function() {
-    $scope.plotData = diffEq.plot();
+    $scope.plotData =  new diffEq.directionalField(
+      $scope.slope_func, 
+      $scope.canvas_width, 
+      $scope.canvas_height
+    );
   });
 });
