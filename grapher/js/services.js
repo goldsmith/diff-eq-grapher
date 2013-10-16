15381bfe5a9efa58c5da7486be3403937354a73c
Jonathan Goldsmith <jhghank@gmail.com>
2013-10-15 23:02:42 -0700

    add plotData and directionalField services

--- grapher/js/services.js
+++ grapher/js/services.js
@@ -1,19 +1,55 @@
 angular.module('grapherApp.services', []).
-  factory("diffEq", function() {
-    return {
-      plot: function() {
+  factory("plotData", function() {
+    return { 
+      new: function() {
         var plotData = {
           points: [],
           lines: []
         };
   
-        plotData.points.push({
-          x: 10,
-          y: 10,
-          r: 10
-        });
+        return {
+          addPoint: function(point) {
+            plotData.points.push(point);
+          },
   
-        return plotData;
+          addLine: function(point1, point2, color, width) {
+            plotData.lines.push({
+              from: point1,
+              to: point2,
+              color: color,
+              width: width || 1.0
+            });
+          },
+  
+          data: plotData
+        }
+      },
+    }
+  }).
+  service("diffEq", function(plotData) {
+    function Point(x, y, r) {
+      return {
+        x: x,
+        y: y,
+        r: r || 1.5
       }
     }
-  });
+
+    this.directionalField = function(slope_func, width, height) {
+      data = plotData.new();
+
+      for (var x = 0; x < width; x += 10) {
+        for (var y = 0; y < height; y += 10) {
+          m = slope_func(x, y);
+          deltax = 5/(Math.sqrt(1 + Math.pow(m, 2)));
+
+          up = Point(x + deltax, y + m*deltax);
+          down = Point(x - deltax, y - m*deltax);
+
+          data.addLine(up, down, 'black');
+        }
+      }
+  
+      return data.data
+    }
+  })
