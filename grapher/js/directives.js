67b966623c4d3bc1c45d44357a4ca081ce707f75
Jonathan Goldsmith <jhghank@gmail.com>
2013-10-15 23:01:25 -0700

    modify draw directive to convert coordinates to HTML5 upside down coordinates and store canvas dimensions in

--- grapher/js/directives.js
+++ grapher/js/directives.js
@@ -3,11 +3,12 @@ angular.module("grapherApp.directives", []).
 
   return function (scope, element, attrs) {
 
-    var ctx = element[0].getContext('2d');
+    var context = element[0],
+        ctx = element[0].getContext('2d');
 
     function drawPoint(point) {
       ctx.beginPath()
-      ctx.arc(point.x, point.y, point.r, 0, Math.PI*2, true)
+      ctx.arc(point.x, context.height - point.y, point.r, 0, Math.PI*2, true)
       ctx.closePath()
       ctx.fill()
     };
@@ -15,8 +16,8 @@ angular.module("grapherApp.directives", []).
     function drawLine(from, to, color, width) {
       ctx.strokeStyle = color;
       ctx.beginPath();
-      ctx.moveTo(from.x, from.y);
-      ctx.lineTo(to.x, to.y);
+      ctx.moveTo(from.x, context.height - from.y);
+      ctx.lineTo(to.x, context.height - to.y);
       ctx.lineWidth = width || 1.0;
       ctx.stroke()
       ctx.lineWidth = 1.0
@@ -26,5 +27,7 @@ angular.module("grapherApp.directives", []).
       drawPoint(scope.plotData.points[0]);
     });
 
+    scope.canvas_width = context.width;
+    scope.canvas_height = context.height;
   }
 });
