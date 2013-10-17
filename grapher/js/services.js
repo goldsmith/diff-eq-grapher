9679a1e970bf6bf188d3c4ae78f0e65d8266fc57
Jonathan Goldsmith <jhghank@gmail.com>
2013-10-17 00:05:55 -0700

    make directional field service functional by distinguishing between bottom left and center based points

--- grapher/js/services.js
+++ grapher/js/services.js
@@ -35,18 +35,48 @@ angular.module('grapherApp.services', []).
       }
     }
 
-    this.directionalField = function(slope_func, width, height) {
+    function CenteredPoint(point, width, height) {
+      // convert a point on a canvas where (0, 0) is
+      // on the bottom left to a point on a canvas where
+      // (0, 0) is in the center
+      return Point(
+        point.x + width/2,
+        point.y + height/2,
+        point.r
+      )
+    }
+
+    function BottomLeftPoint(point, width, height) {
+      // convert a point on a canvas where (0, 0) is
+      // in the center to a point on a canvas where
+      // (0, 0) is on the bottom left corner
+      return Point(
+        point.x - width/2,
+        point.y - height/2,
+        point.r
+      )
+    }
+
+    this.directionalField = function(slope_func, width, height, scale) {
       data = plotData.new();
 
       for (var x = 0; x < width; x += 10) {
         for (var y = 0; y < height; y += 10) {
-          m = slope_func(x, y);
+          // center the coordinate axes on the plot
+          point = BottomLeftPoint(Point(x, y), width, height);
+
+          m = slope_func(point.x/scale, point.y/scale);
           deltax = 5/(Math.sqrt(1 + Math.pow(m, 2)));
 
-          up = Point(x + deltax, y + m*deltax);
-          down = Point(x - deltax, y - m*deltax);
+          up = Point(point.x + deltax, point.y + m*deltax);
+          down = Point(point.x - deltax, point.y - m*deltax);
 
-          data.addLine(up, down, 'black');
+          // reattach point to display coordinates
+          data.addLine(
+            CenteredPoint(up, width, height),
+            CenteredPoint(down, width, height),
+            'black'
+          );
         }
       }
   
