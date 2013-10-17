27e36cca65286b024a4779a89401bc519a6e9441
Jonathan Goldsmith <jhghank@gmail.com>
2013-10-17 15:37:52 -0700

    refactor services to distinguish between display points and coordinate points and add utils module

--- grapher/js/services.js
+++ grapher/js/services.js
@@ -1,21 +1,31 @@
 angular.module('grapherApp.services', []).
-  factory("plotData", function() {
-    return { 
-      new: function() {
+  service("utils", function() {
+    this.Point = function(x, y, r) {
+      return {
+        x: x,
+        y: y,
+        r: r || 1.5
+      }
+    };
+  }).
+  service("plotData", function(utils) {
+    var Point = utils.Point;
+
+    this.new = function(width, height) {
         var plotData = {
           points: [],
           lines: []
         };
-  
+
         return {
           addPoint: function(point) {
-            plotData.points.push(point);
+            plotData.points.push(CenteredPoint(point));
           },
   
           addLine: function(point1, point2, color, width) {
             plotData.lines.push({
-              from: point1,
-              to: point2,
+              from: CenteredPoint(point1),
+              to: CenteredPoint(point2),
               color: color,
               width: width || 1.0
             });
@@ -23,47 +33,33 @@ angular.module('grapherApp.services', []).
   
           data: plotData
         }
-      },
-    }
-  }).
-  service("diffEq", function(plotData) {
-    function Point(x, y, r) {
-      return {
-        x: x,
-        y: y,
-        r: r || 1.5
-      }
-    }
 
-    function CenteredPoint(point, width, height) {
-      // convert a point on a canvas where (0, 0) is
-      // on the bottom left to a point on a canvas where
-      // (0, 0) is in the center
-      return Point(
-        point.x + width/2,
-        point.y + height/2,
-        point.r
-      )
+        function CenteredPoint(point) {
+          // convert a point for display
+          // from a centered coordinate axis
+          // to the canvas element
+          return Point(
+            point.x + width/2,
+            point.y + height/2,
+            point.r
+          )
+        }
+      };
     }
-
-    function BottomLeftPoint(point, width, height) {
-      // convert a point on a canvas where (0, 0) is
-      // in the center to a point on a canvas where
-      // (0, 0) is on the bottom left corner
-      return Point(
-        point.x - width/2,
-        point.y - height/2,
-        point.r
-      )
-    }
+  ).
+  service("diffEq", function(plotData, utils) {
+    var Point = utils.Point;
 
     this.directionalField = function(slope_func, width, height, scale) {
-      data = plotData.new();
+      data = plotData.new(width, height);
 
       for (var x = 0; x < width; x += 10) {
         for (var y = 0; y < height; y += 10) {
           // center the coordinate axes on the plot
-          point = BottomLeftPoint(Point(x, y), width, height);
+          point = Point(
+            x - width/2,
+            y - height/2
+          );
 
           m = slope_func(point.x/scale, point.y/scale);
           deltax = 5/(Math.sqrt(1 + Math.pow(m, 2)));
@@ -71,12 +67,7 @@ angular.module('grapherApp.services', []).
           up = Point(point.x + deltax, point.y + m*deltax);
           down = Point(point.x - deltax, point.y - m*deltax);
 
-          // reattach point to display coordinates
-          data.addLine(
-            CenteredPoint(up, width, height),
-            CenteredPoint(down, width, height),
-            'black'
-          );
+          data.addLine(up, down, 'black');
         }
       }
   
@@ -84,7 +75,7 @@ angular.module('grapherApp.services', []).
     };
 
     this.eulerApproximation = function(y_0, slope_func, step_size, width, height, scale) {
-      var data = plotData.new();
+      var data = plotData.new(width, height);
 
       var left = Point(0, 0),
           right = Point(0, 0);
@@ -95,12 +86,12 @@ angular.module('grapherApp.services', []).
       }
 
       function _eulerApprox(point, step_size) {
-        data.addPoint(CenteredPoint(point, width, height));
+        data.addPoint(point);
         new_point = Point(
           point.x + step_size,
           point.y + step_size * slope_func(point.x/scale, point.y/scale) 
         );
-        data.addLine(CenteredPoint(point, width, height), CenteredPoint(new_point, width, height), 'blue');
+        data.addLine(point, new_point, 'blue');
         return new_point
       }
 
